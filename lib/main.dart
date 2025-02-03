c80dc1f237922bfff597e672454d0a08abe68189
aprdec <1263868407@qq.com>
2025-02-03 20:57:08 +0800

    settingpage

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,11 @@ import 'package:chargestation/navigatorpage.dart';
 import 'package:chargestation/page/Home.dart';
 import 'package:chargestation/page/SettingsPage.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
 
-void main() {
+void main() async{
   runApp(GetMaterialApp(
       home: navigatorpage(),
       defaultTransition: Transition.fade,
@@ -14,6 +15,13 @@ void main() {
         GetPage(name: '/home', page: ()=>Home()),
         GetPage(name: '/setting', page: ()=>SettingsPage())
       ],
+    locale: const Locale("zh", "CN"),
+    localizationsDelegates: const [
+      GlobalMaterialLocalizations.delegate,
+      GlobalWidgetsLocalizations.delegate,
+      GlobalCupertinoLocalizations.delegate,
+    ],
+    supportedLocales: const [Locale("zh", "CN")],
       navigatorObservers: [FlutterSmartDialog.observer],
       builder: FlutterSmartDialog.init(),
   ));
--- lib/page/SettingsPage.dart
+++ lib/page/SettingsPage.dart
@@ -1,60 +1,90 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_smart_dialog/flutter_smart_dialog.dart';
 import 'package:get/get.dart';
+import 'package:simple_icons/simple_icons.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class SettingsPage extends StatelessWidget {
 
-  final Uri _url = Uri.parse('https://www.aprdec.top');
-  Future<void> _launchUrl() async {
-    if (!await launchUrl(_url)) {
-      throw Exception('Could not launch $_url');
+  final Uri url_blog = Uri.parse('https://www.aprdec.top');
+  final Uri url_github = Uri.parse('https://github.com/aprdeci');
+
+  Future<void> _launchUrl(url) async {
+    if (!await launchUrl(url)) {
+      throw Exception('不能打开 $url');
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('设置'),
-      ),
-      body: ListView(
+    return SafeArea(
+      child: ListView(
+        padding: EdgeInsets.all(4),
         children: <Widget>[
-          Card(
-            elevation: 4.0,
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(10.0),
-            ),
-            child: Center(
-              child: Container(
-                padding: EdgeInsets.all(16.0),
-                child: CircleAvatar(
-                  radius: 30,
-                  backgroundImage: NetworkImage('https://www.helloimg.com/i/2025/01/09/677f75ebe7543.jpg'),
+          ListTile(
+            leading: Image.asset(
+              'assets/love.jpg',
+              width: 56,
+              height: 56,),
+            title: Text('河农大充电桩查看器',style: TextStyle(height: 1.0,fontSize: 16)),
+            subtitle: Text('求求学弟学妹了给个star吧',style: TextStyle(height: 1.0,fontSize: 10),),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: (){
+              Get.dialog(AboutDialog(
+                applicationIcon: Image.asset(
+                  'assets/love.jpg',
+                  width: 48,
+                  height: 48,
                 ),
-              ),
-            )
+                applicationName: "河农大充电桩查看器",
+                applicationVersion: "简简单单看直播",
+                applicationLegalese: "Ver 1.1",
+              ));
+            },
+          ),
+          Divider(
+            indent: 12,
+            endIndent: 12,
+            color: Colors.grey.withOpacity(.1),
           ),
           ListTile(
-            leading: Icon(Icons.abc),
-            title: Text('可能以后会更新,所以加一个这个页面备用吧,点击打开我的博客哦(www.aprdec.top)'),
-            onTap: () {
-              _launchUrl();
+            leading: Icon(SimpleIcons.github,color: SimpleIconColors.github,),
+            title: Text('项目地址'),
+            trailing: Icon(Icons.keyboard_arrow_right),
+            onTap: (){
+              _launchUrl(url_github);
             },
+          ),
+          ListTile(
+            leading: Icon(SimpleIcons.notion,color: SimpleIconColors.notion),
+            title: Text('学长博客'),
+            onTap: () {
+              _launchUrl(url_blog);
+            },
+            trailing: Icon(Icons.keyboard_arrow_right),
           ),
           ListTile(
             leading: Icon(Icons.accessibility_new_rounded),
             title: Text('感谢名单'),
             onTap: () {
-              SmartDialog.show(
-                  builder:(context){
-                return Card(
-                    child: Container(
-                      padding: EdgeInsets.all(16),
-                      child: Text("软件21-3班李继鹏,感谢他提供兰亭苑设备ID名单"),
-                    )
-                );});
+             Get.dialog(
+               AlertDialog(
+                 title: Text('感谢名单'),
+                 content: Text('谢谢软件21-3班李继鹏和我一起扫码记录ID'),
+                 actions: [
+                   TextButton(
+                     onPressed: (() => Get.back(result: false)),
+                     child: Text("取消"),
+                   ),
+                   TextButton(
+                     onPressed: (() => Get.back(result: true)),
+                     child: Text("确定"),
+                   ),
+                 ],
+               )
+             );
             },
+            trailing: Icon(Icons.keyboard_arrow_right),
           ),
         ],
       ),
